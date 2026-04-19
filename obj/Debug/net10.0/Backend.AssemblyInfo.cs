41983f2532951a5f438f490ae5f9eea083733b26
Sandile <mabilisasandile@gmail.com>
2026-04-19 05:59:13 +0200

    Add Dockerfile for Render

--- obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa723949f059aa9f96fb8ce700aae971c7a63fad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+583122b92930d35a47a6c16527be6ddbf3e7bb51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
